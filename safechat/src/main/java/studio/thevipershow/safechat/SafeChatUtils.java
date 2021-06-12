06ce4092105cd7c94717d58be32ce8d779c1f600
Joshua <Jochyoua@gmail.com>
2021-06-12 16:42:56 -0500

    Fixed bug
    Added fallback regex for swears
    Added logging for checks, added toggle in API and config files

--- safechat/src/main/java/studio/thevipershow/safechat/SafeChatUtils.java
+++ safechat/src/main/java/studio/thevipershow/safechat/SafeChatUtils.java
@@ -1,28 +1,81 @@
 package studio.thevipershow.safechat;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.tomlj.TomlArray;
-import studio.thevipershow.safechat.SafeChat;
+import studio.thevipershow.safechat.api.checks.ChatCheck;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public final class SafeChatUtils {
+
+    public static final char DEFAULT_COLOR_CHAR = 0x26;
+    private static CommandMap commandMap;
 
     private SafeChatUtils() {
         throw new UnsupportedOperationException("Cannot instantiate utility class.");
     }
 
-    public static final char DEFAULT_COLOR_CHAR = 0x26;
-
     @NotNull
     public static String color(@NotNull String string) {
         return ChatColor.translateAlternateColorCodes(DEFAULT_COLOR_CHAR, string);
+    }
+
+    public static void logMessage(ChatCheck aClass, Player player, String message) {
+        if (!aClass.getLoggingEnabled()) {
+            return;
+        }
+        Logger logger = Logger.getLogger(aClass.getName() + " check");
+        try {
+            File file = new File(SafeChat.getPlugin(SafeChat.class).getDataFolder(), "logs/checkLogs.log");
+            if (!file.exists() && !file.createNewFile()) {
+                return;
+            }
+            FileHandler fileHandler = new FileHandler(file.getPath(), true);
+            fileHandler.setFormatter(new SimpleFormatter() {
+                @Override
+                public String format(LogRecord logRecord) {
+                    ZonedDateTime zdt = ZonedDateTime.ofInstant(
+                            Instant.ofEpochMilli(logRecord.getMillis()), ZoneId.systemDefault());
+                    String source;
+                    source = logRecord.getLoggerName();
+                    String message = formatMessage(logRecord);
+                    return String.format("%1$tb %1$td, %1$tY %1$tl:%1$tM:%1$tS %1$Tp %2$s%n%4$s: %5$s%n",
+                            zdt,
+                            source,
+                            logRecord.getLoggerName(),
+                            logRecord.getLevel().getLocalizedName(),
+                            message);
+                }
+            });
+
+            logger.addHandler(fileHandler);
+            logger.setUseParentHandlers(false);
+            logger.setLevel(Level.INFO);
+
+            logger.log(Level.INFO, player.getName() + ": " + message + "\n");
+            fileHandler.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @NotNull
@@ -44,8 +97,6 @@ public final class SafeChatUtils {
         }
     }
 
-    private static CommandMap commandMap;
-
     /**
      * This methods allows to use the CommandMap on different forks
      * This method has been provided by electroniccat , thanks to him!
@@ -63,4 +114,5 @@ public final class SafeChatUtils {
         }
         return commandMap;
     }
+
 }
